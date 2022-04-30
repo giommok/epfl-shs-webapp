67b726043dc59f8911a5cf8ba2f2c8db81ade168
giommok <giovanni.monea@epfl.ch>
2022-04-30 16:44:46 +0200

    fixed crash when no option selected

--- forms.py
+++ forms.py
@@ -1,8 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import RadioField, SubmitField, StringField, validators
 
+class FieldsRequiredForm(FlaskForm):
+    """Require all fields to have content. This works around the bug that WTForms radio
+    fields don't honor the `DataRequired` or `InputRequired` validators.
+    """
 
-class QuizForm(FlaskForm):
+    class Meta:
+        def render_field(self, field, render_kw):
+            render_kw.setdefault('required', True)
+            return super().render_field(field, render_kw)
+
+class QuizForm(FieldsRequiredForm):
     question = RadioField('choice', [validators.Required()])
     submit = SubmitField('Submit')
 
